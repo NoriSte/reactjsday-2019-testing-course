c8aecae4446f9c961c8ee588f4b302fb493ab9ef
NoriSte <nori.ste.magni@gmail.com>
2019-08-20 18:42:16 +0200

    Change the server port to 3100

--- realworld/backend/app.js
+++ realworld/backend/app.js
@@ -79,6 +79,6 @@ app.use(function(err, req, res, next) {
 });
 
 // finally, let's start our server...
-var server = app.listen( process.env.PORT || 3000, function(){
+var server = app.listen( process.env.PORT || 3100, function(){
   console.log('Listening on port ' + server.address().port);
 });
