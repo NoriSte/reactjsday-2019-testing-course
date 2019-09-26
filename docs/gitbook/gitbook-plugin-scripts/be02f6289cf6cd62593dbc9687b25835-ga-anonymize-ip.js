676820c3b59fd35a20d787d2616fb334a254cc09
NoriSte <nori.ste.magni@gmail.com>
2019-09-26 10:14:26 +0200

    Publish the book

--- docs/gitbook/gitbook-plugin-scripts/be02f6289cf6cd62593dbc9687b25835-ga-anonymize-ip.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", function() {
-  var intervalId = setInterval(function() {
-    if (ga) {
-      ga("set", "anonymizeIp", true);
-      clearInterval(intervalId);
-    }
-  }, 100);
-});
